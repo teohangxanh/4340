3bb1a8727cb6aa250a8a235b1d7c908a321d255e
teohangxanh <47458301+teohangxanh@users.noreply.github.com>
2020-03-14 16:54:10 -0600

    A Jupyter notebook was created

--- Project.py
+++ Project.py
@@ -13,16 +13,16 @@ signs = '|'.join(['€', 'M', 'K'])
 x['Value'] = x['Value'].str.replace(signs, '')
 x['Wage'] = x['Wage'].str.replace(signs, '')
 x['Release Clause'] = x['Release Clause'].str.replace(signs, '')
+x = x.rename(columns={'Wage': 'Wage_in_K', 'Value': 'Value_in_M', 'Release Clause': 'Release_clause_in_M'})
 x['Work Rate'] = x['Work Rate'].replace(['Low/ Low', 'Low/ Medium', 'Low/ High','Medium/ Low', 'Medium/ Medium', 'Medium/ High',
                               'High/ Low', 'High/ Medium', 'High/ High'], 
                               ['2', '3', '4', '3', '4', '5', '4', '5', '6'])
-x = x.astype({'Age': 'int8', 'Overall': 'int8', 'Special': 'int8', 'Work Rate': 'int8', 'Value': 'float16', 'Wage': 'float16', 'Release Clause': 'float16'})
-x = x.rename(columns={'Wage': 'Wage_in_K', 'Value': 'Value_in_M', 'Release Clause': 'Release_clause_in_M'})
+x = x.astype({'Age': 'int8', 'Overall': 'int8', 'Special': 'int16', 'Work Rate': 'int8', 'Wage_in_K': 'float16', 'Value_in_M': 'float16', 'Release_clause_in_M': 'float16'})
 
 # Turn Preferred Foot into 0 and 1
 x = pd.get_dummies(x, columns=['Preferred Foot'], drop_first=True)
 # Turn Preferred Foot_Right into int
-x['Preferred Foot_Right'] = x['Preferred Foot_Right'].astype('category')
+x['Preferred Foot_Right'] = x['Preferred Foot_Right'].astype('int8')
 
 # Handle "+" from column LS:RB
 for val in x.loc[:, 'LS':'RB']:
@@ -37,16 +37,25 @@ col_del_from = x.columns.get_loc('LS_0')
 col_del_to = x.columns.get_loc('RB_1')
 x.drop(x.columns[col_del_from: col_del_to + 1], axis=1, inplace=True)
 
-y = x.iloc[:, 3]
+# Add series y, which is the target for the predicting model
+y = x.loc[:, 'Value_in_M']
+# Drop column 
 x.drop(columns = 'Value_in_M', axis=1)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc_x = StandardScaler()
-x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)
+
+
+
+
+
+
+
+
+
+
+
+
 
